2312951e0e9efe6cf985d87790aa9811aef46936
Kevin7274 <kuhes0918@gmail.com>
2022-06-29 20:17:34 -0500

    no repetir codigo

---
+++ funciones.py
@@ -0,0 +1,25 @@
+#def imprimir mensaje():
+#    print('mensaje especial')
+#    print('¡estoy aprendiendo a hacer funciones!')
+
+
+# imprimir_mensaje()
+# imprimir_mensaje()
+# imprimir_mensaje()
+
+opcion = int(input('Elige una opcion (1,2,3): '))
+if opcion == 1:
+    print('hola')
+    print('cómo estas')
+    print('elegiste la opcion 1')
+    print('adios')
+elif opcion == 2:
+     print('hola')
+     print('cómo estas')
+     print('elegiste la opcion 2')
+     print('adios')
+elif opcion == 3:
+     print('hola')
+     print('cómo estas')
+     print('elegiste la opcion 3')
+     print('adios')
