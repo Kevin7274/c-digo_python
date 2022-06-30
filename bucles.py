c5c6f9d6dfe956dcb6157b3338576f568b7dc197
Kevin7274 <kuhes0918@gmail.com>
2022-06-29 20:59:44 -0500

    aprendiendo bucles

---
+++ bucles.py
@@ -0,0 +1,16 @@
+contador = 0
+print('2 elevado a ' + str(contador) + ' es iguala: ' + str(2**contador))
+
+contador = 1
+print('2 elevado a ' + str(contador) + ' es iguala: ' + str(2**contador))
+
+contador = 2
+
+contador = 3
+print('2 elevado a ' + str(contador) + ' es iguala: ' + str(2**contador))
+
+contador = 4
+print('2 elevado a ' + str(contador) + ' es iguala: ' + str(2**contador))
+
+contador = 5
+print('2 elevado a ' + str(contador) + ' es iguala: ' + str(2**contador))
